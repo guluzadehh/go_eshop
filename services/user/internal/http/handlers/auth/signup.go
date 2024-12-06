d361596c9a74dd188c1278cf1af608fc3a4229c1
guluzadehh <quluzade.resul13@mail.ru>
2024-12-07 03:21:59 +0400

    Add signup request validation

--- services/user/internal/http/handlers/auth/signup.go
+++ services/user/internal/http/handlers/auth/signup.go
@@ -4,18 +4,20 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	requestmdw "github.com/guluzadehh/go_eshop/services/user/internal/http/middlewares/request"
 	"github.com/guluzadehh/go_eshop/services/user/internal/lib/api"
 	"github.com/guluzadehh/go_eshop/services/user/internal/lib/render"
 	"github.com/guluzadehh/go_eshop/services/user/internal/lib/sl"
+	"github.com/guluzadehh/go_eshop/services/user/internal/lib/validators"
 	"github.com/guluzadehh/go_eshop/services/user/internal/services/auth"
 	"github.com/guluzadehh/go_eshop/services/user/internal/types"
 )
 
 type SignupReq struct {
-	Email        string `json:"email"`
-	Password     string `json:"password"`
-	ConfPassword string `json:"conf_password"`
+	Email        string `json:"email" validate:"required,email"`
+	Password     string `json:"password" validate:"required,min=5,passwordpattern"`
+	ConfPassword string `json:"conf_password" validate:"required,eqfield=Password"`
 }
 
 type SignupRes struct {
@@ -39,7 +41,21 @@ func (h *AuthHandler) Signup(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// TODO: validate
+	v := validator.New()
+	v.RegisterValidation("passwordpattern", validators.PasswordPatternValidator)
+
+	if err := v.StructCtx(r.Context(), &req); err != nil {
+		validateErr, ok := err.(validator.ValidationErrors)
+		if !ok {
+			log.Error("error happened while validating request", sl.Err(err))
+			h.JSON(w, http.StatusInternalServerError, api.UnexpectedError())
+			return
+		}
+
+		log.Info("invalid request")
+		h.JSON(w, http.StatusBadRequest, api.ValidationError(validateErr))
+		return
+	}
 
 	user, err := h.srvc.Signup(r.Context(), req.Email, req.Password)
 	if err != nil {
