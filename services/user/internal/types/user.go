99585890fb8e7dbf889083401941090afe86a742
guluzadehh <quluzade.resul13@mail.ru>
2024-12-10 23:33:53 +0400

    Add new fields to UserView

--- services/user/internal/types/user.go
+++ services/user/internal/types/user.go
@@ -1,10 +1,17 @@
 package types
 
-import "github.com/guluzadehh/go_eshop/services/user/internal/domain/models"
+import (
+	"time"
+
+	"github.com/guluzadehh/go_eshop/services/user/internal/domain/models"
+)
 
 type UserView struct {
-	Id    int64  `json:"id"`
-	Email string `json:"email"`
+	Id        int64     `json:"id"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	IsActive  bool      `json:"is_active"`
 }
 
 func NewUser(u *models.User) *UserView {
@@ -13,7 +20,10 @@ func NewUser(u *models.User) *UserView {
 	}
 
 	return &UserView{
-		Id:    u.Id,
-		Email: u.Email,
+		Id:        u.Id,
+		Email:     u.Email,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+		IsActive:  u.IsActive,
 	}
 }
